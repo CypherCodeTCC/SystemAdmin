8a292466666caeefa736961cf5cf40f6566bb42e
Juan <juanpfreire18@gmail.com>
2024-06-18 16:51:27 -0300

    [refactor] changes to the burger menu

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,18 +1,69 @@
+import { useState, useEffect } from "react";
 import PngLogo from "../../../public/logotipo.png";
 import PngUser from "../../../public/user.png";
-import MobileMenu from "./mobile/MobileMenu";
-import { Container } from "./navBarStyle";
+import {
+  ALiBotao,
+  Container,
+  DropUser,
+  LiBotao,
+  List,
+  MenuMobile,
+} from "./navBarStyle";
 
 export default function NavBar() {
+  const [userMenuOpen, setUserMenuOpen] = useState(false);
+
+  const toggleUserMenu = () => {
+    setUserMenuOpen(!userMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1150 && userMenuOpen) setUserMenuOpen(false);
+    };
+
+    window.addEventListener("resize", handleResize);
+  }, [userMenuOpen]);
+
+  console.log(userMenuOpen);
   return (
     <>
-      <Container>
+      <Container className="container">
         <div className="container-logo">
           <img src={PngLogo} alt="Logotipo da empresa" />
         </div>
         <div className="container-icons">
           <img src={PngUser} alt="Botão para logar" />
-          <MobileMenu />
+          <MenuMobile>
+            {!userMenuOpen ? (
+              <span
+                id="burger"
+                className="material-symbols-outlined"
+                onClick={toggleUserMenu}
+              >
+                menu
+              </span>
+            ) : (
+              <span
+                id="close"
+                className="material-symbols-outlined"
+                onClick={toggleUserMenu}
+              >
+                close
+              </span>
+            )}
+          </MenuMobile>
+          <DropUser isOpen={userMenuOpen}>
+            <List>
+              <li>Excluir Autores</li>
+              <li>Excluir Gêneros</li>
+              <li>Excluir Livros</li>
+              <li>Excluir Autores</li>
+              <LiBotao>
+                <ALiBotao>Logout</ALiBotao>
+              </LiBotao>
+            </List>
+          </DropUser>
         </div>
       </Container>
     </>
--- src/components/nav/navBarStyle.jsx
+++ src/components/nav/navBarStyle.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.nav`
+  position: relative;
   display: flex;
   background-color: #ffffff;
   justify-content: space-between;
@@ -29,4 +30,67 @@ export const Container = styled.nav`
       width: 40%;
     }
   }
+`;
+
+export const MenuMobile = styled.div`
+  display: none;
+
+  @media (max-width: 1024px) {
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const DropUser = styled.div`
+  text-align: left;
+  min-height: 100%;
+  font-size: 24px;
+  position: absolute;
+  top: calc(100% + 10px);
+  left: 50%;
+  transform: translateX(-50%);
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 16px;
+  width: 100%;
+  padding: 30px;
+  z-index: 1001;
+`;
+
+export const List = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  gap: 100px;
+`;
+
+export const LiBotao = styled.li`
+  flex: 1 1 min-content;
+`;
+
+export const ALiBotao = styled.a`
+  background: #000000;
+  color: #ffffff;
+  display: inline-block;
+  border-radius: 10px;
+  padding: 14px 24px 16px;
+  font-size: 1.125rem;
+  font-weight: 700;
+  line-height: 1;
+  border: 0;
+  user-select: none;
+  text-align: center;
+  text-decoration: none;
+  position: relative;
+  background: transparent;
+  color: #000000;
+  box-shadow: 0 0 0 3px #000000 inset;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  font-size: 1rem;
 `;
