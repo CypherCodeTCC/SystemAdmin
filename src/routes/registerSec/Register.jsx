5a61a9120796451ece817cb43e3eba262dec8077
Juan <juanpfreire18@gmail.com>
2024-06-20 15:45:29 -0300

    [feat] creating employee insertion

--- src/routes/registerSec/Register.jsx
+++ src/routes/registerSec/Register.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import {
   AList,
-  ButtonProfile,
   Container,
   ContainerButtonProfile,
   ContainerOptions,
@@ -15,6 +14,8 @@ import {
 } from "../panelSec/panelStyle";
 import { ButtonRegister, Input, Options } from "./registerStyle";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export default function Register() {
   const navigate = useNavigate();
@@ -30,11 +31,11 @@ export default function Register() {
     Senha: "",
     CEP: "",
     Logradouro: "",
-    Numero: "",
+    NumeroEnd: "",
     Complemento: "",
-    Cidade: "",
-    Estado: "",
-  })
+    NomeCid: "",
+    Uf: "",
+  });
 
   //ALTERA A ANIMAÇÃO DAS OPÇÕES DA LISTA DO FORMULARIO
   const changeAnimation = (id) => {
@@ -49,17 +50,35 @@ export default function Register() {
   //ALTERA A COR DA LETRA DAS OPÇÕES
   const changeOptionColor = (id) => {
     setOptionColor(id === optionColor ? id : id);
-    navigate("/panel", {state: {optionColor: id}});
+    navigate("/panel", { state: { optionColor: id } });
   };
 
   const handleContinue = () => {
     changeAnimation(2);
     changeColorInsert(2);
-  }
+  };
 
   const handleChange = (e) => {
-    setAdmin((prev) => ({...prev, [e.target.name] : e.target.value}));
-  }
+    setAdmin((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      await axios.post("https://node-routes-mysql.vercel.app/admin", admin);
+      toast.success("Funcionário cadastrado com sucesso!", {
+        closeOnClick: true,
+      });
+      navigate("/panel");
+    } catch (err) {
+      toast.error(
+        "Erro ao cadastrar o funcionário. Tente novamente mais tarde",
+        {
+          closeOnClick: true,
+        }
+      );
+      console.log("Erro ao cadastrar o funcionário.", err);
+    }
+  };
 
   const renderForm = () => {
     switch (isActive) {
@@ -70,31 +89,62 @@ export default function Register() {
               <Profile>
                 <Infos>
                   <p>Nome Completo</p>
-                  <Input type="text" name="Nome" value={admin.Nome} placeholder="Digite o nome completo" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="Nome"
+                    value={admin.Nome}
+                    placeholder="Digite o nome completo"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>CPF</p>
-                  <Input type="text" name="CPF" value={admin.CPF} placeholder="Digite o CPF" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="CPF"
+                    value={admin.CPF}
+                    placeholder="Digite o CPF"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>Email</p>
-                  <Input type="text" name="Email" value={admin.Email} placeholder="Digite o email" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="Email"
+                    value={admin.Email}
+                    placeholder="Digite o email"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>Telefone</p>
-                  <Input type="text" name="Telefone" value={admin.Telefone} placeholder="Digite o telefone" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="Telefone"
+                    value={admin.Telefone}
+                    placeholder="Digite o telefone"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>Senha</p>
-                  <Input type="password" name="Senha" value={admin.Senha} placeholder="Digite a senha" onChange={handleChange}/>
+                  <Input
+                    type="password"
+                    name="Senha"
+                    value={admin.Senha}
+                    placeholder="Digite a senha"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <ContainerButtonProfile>
                   {!admin.Nome ? (
                     <ButtonRegister isActive={false}>Continuar</ButtonRegister>
                   ) : (
-                    <ButtonRegister onClick={handleContinue} isActive={true}>Continuar</ButtonRegister>
+                    <ButtonRegister onClick={handleContinue} isActive={true}>
+                      Continuar
+                    </ButtonRegister>
                   )}
-                  
                 </ContainerButtonProfile>
               </Profile>
             </Fields>
@@ -107,30 +157,68 @@ export default function Register() {
               <Profile>
                 <Infos>
                   <p>CEP</p>
-                  <Input type="text" name="CEP" value={admin.CEP} placeholder="Digite o CEP" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="CEP"
+                    value={admin.CEP}
+                    placeholder="Digite o CEP"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>Logradouro</p>
-                  <Input type="text" name="Logradouro" value={admin.Logradouro} placeholder="Digite o logradouro" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="Logradouro"
+                    value={admin.Logradouro}
+                    placeholder="Digite o logradouro"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>Número</p>
-                  <Input type="text" name="Numero" value={admin.Numero} placeholder="Digite o número" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="NumeroEnd"
+                    value={admin.NumeroEnd}
+                    placeholder="Digite o número"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>Complemento</p>
-                  <Input type="text" name="Complemento" value={admin.Complemento} placeholder="Digite o complemento" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="Complemento"
+                    value={admin.Complemento}
+                    placeholder="Digite o complemento"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>Cidade</p>
-                  <Input type="text" name="Cidade" value={admin.Cidade} placeholder="Digite a cidade" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="NomeCid"
+                    value={admin.NomeCid}
+                    placeholder="Digite a cidade"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <Infos>
                   <p>Estado</p>
-                  <Input type="text" name="Estado" value={admin.Estado} placeholder="Digite o estado" onChange={handleChange}/>
+                  <Input
+                    type="text"
+                    name="Uf"
+                    value={admin.Uf}
+                    placeholder="Digite o estado"
+                    onChange={handleChange}
+                  />
                 </Infos>
                 <ContainerButtonProfile>
-                  <ButtonRegister>Cadastrar</ButtonRegister>
+                  <ButtonRegister onClick={handleSubmit} isActive={true}>
+                    Cadastrar
+                  </ButtonRegister>
                 </ContainerButtonProfile>
               </Profile>
             </Fields>
@@ -145,10 +233,20 @@ export default function Register() {
         <ContainerOptions>
           <ListOptions>
             <LiOptions>
-              <Options onClick={() => changeOptionColor(1)} className={optionColor === 1 ? "blue" : ""}>Overview</Options>
+              <Options
+                onClick={() => changeOptionColor(1)}
+                className={optionColor === 1 ? "blue" : ""}
+              >
+                Overview
+              </Options>
             </LiOptions>
             <LiOptions>
-              <Options onClick={() => changeOptionColor(2)} className={optionColor === 2 ? "blue" : ""}>Configurações</Options>
+              <Options
+                onClick={() => changeOptionColor(2)}
+                className={optionColor === 2 ? "blue" : ""}
+              >
+                Configurações
+              </Options>
             </LiOptions>
           </ListOptions>
         </ContainerOptions>
@@ -165,12 +263,8 @@ export default function Register() {
                 Dados
               </AList>
             </li>
-            <li
-              className={isActive === 2 ? "active" : ""}
-            >
-              <AList
-                className={insertColor === 2 ? "blue" : ""}
-              >
+            <li className={isActive === 2 ? "active" : ""}>
+              <AList className={insertColor === 2 ? "blue" : ""}>
                 Endereço
               </AList>
             </li>
